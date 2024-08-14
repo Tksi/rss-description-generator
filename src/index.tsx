36e751b178ea10cc8ca7a9dfe25dda0dd28caf61
Tksi <24800668+Tksi@users.noreply.github.com>
2024-08-14 16:32:24 +0900

    feat: test KV

--- src/index.tsx
+++ src/index.tsx
@@ -13,4 +13,11 @@ app.get('/', (c) => {
   return c.render(<h1>Hello!</h1>);
 });
 
+app.get('/kv', async (c) => {
+  await c.env.KV.put('name', 'test');
+  const name = await c.env.KV.get('name');
+
+  return c.render(<h1>Hell! {name}</h1>);
+});
+
 export default app;
